5da4abbaae335df5fdaca6fe7df5a8e1f4c692ce
root <root@mbskk>
2023-06-14 23:25:21 +0530

    first commit

---
+++ DevOps/src/main/java/Alpha/com/App.java
@@ -0,0 +1,13 @@
+package Alpha.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to DIJ" );
+    }
+}
